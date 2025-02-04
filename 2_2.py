39bbefef826e2a05a01b2ff6582467a2f3231f04
Ashwith <69070712+ashwith0111@users.noreply.github.com>
2025-02-04 08:55:29 +0530

    Update 2_2.py

--- 2_2.py
+++ 2_2.py
@@ -1,16 +1,14 @@
+print("ENTER STOP WHEN COMPLETED")
+pn = input("Give the name of product: ")
 dict1 = {}
-pn = input("Give the product name: ")
-pr = int(input("Give the product price: "))
-while(pn != "STOP"):
+while(pn!="STOP"):
+    pr = int(input("Give the price of the product: "))
     dict1[pn] = pr
-    pn = input("Give the product name: ")
-    pr = int(input("Give the product price: "))
-search = input("Give the product name to search: ")
-while(search != "STOP"):
-    if search in dict1:
-        print(f"the price of the product is {dict1[search]}")
-    else:
-        print("Give string is not present in the product name's")
-    search = input("Give the product name to search: ")
+    pn = input("Give the name of product: ")
+print("ENTER STOP WHEN COMPLETED")
+pn = input("Give the name of product to be searched: ")
+while(pn!="STOP"):
+    print(f"price of {pn} is {dict1[pn]}")
+    pn = input("Give the name of product to be searched: ")
 print("Thank you")
     
